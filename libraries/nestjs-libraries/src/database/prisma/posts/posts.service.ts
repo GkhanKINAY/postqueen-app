gitroomhq/postiz-app

feat(orchestrator): post workflow v1.1.2 retries on heartbeat timeout with no heartbeat details
Replaces the v1.1.1 timing window with the timeout failure's lastHeartbeatDetails.

libraries/nestjs-libraries/src/database/prisma/posts/posts.service.ts
@@ -727,7 +727,7 @@ export class PostsService {
     try {
       await this._temporalService.client
         .getRawClient()
-        ?.workflow.start('postWorkflowV111', {
+        ?.workflow.start('postWorkflowV112', {
           workflowId: `post_${postId}`,
           taskQueue: 'main',
           workflowIdConflictPolicy: 'TERMINATE_EXISTING',

GkhanKINAY/postqueen-app

Adapted for this fork: upstream's post.workflow.v1.1.2.ts lands as our
post.workflow.v1.0.8.ts. Our v1.0.7 already holds upstream's v1.0.6, and
none of upstream's v1.0.7 to v1.1.1 ever ran here, so this one file carries
everything those versions changed: activity heartbeats, the dropped and
restored heartbeat timeout, repeat-post re-anchoring, and the retries on
heartbeat timeouts. Three lines differ from upstream's file: the generated
Prisma client import, the function name (postWorkflowV108) and the
startChild that re-queues a repeat post.
posts.service.ts and the missed-posts sweep in post.activity.ts now start
postWorkflowV108. v1.0.5 to v1.0.7 stay exported for runs already in
flight.
(cherry picked from commit 3e6206f7b3a7116031054a40647ebceb765bdafe)

libraries/nestjs-libraries/src/database/prisma/posts/posts.service.ts
@@ -773,7 +773,7 @@ export class PostsService {
     try {
       await this._temporalService.client
         .getRawClient()
-        ?.workflow.start('postWorkflowV107', {
+        ?.workflow.start('postWorkflowV108', {
           workflowId: `post_${postId}`,
           taskQueue: 'main',
           workflowIdConflictPolicy: 'TERMINATE_EXISTING',