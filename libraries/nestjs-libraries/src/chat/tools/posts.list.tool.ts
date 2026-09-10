gitroomhq/postiz-app

fix(agent): state in tool metadata that posts cannot be deleted
An external MCP agent (Claude Code), right after listing posts, offered
the user to delete one - there is no delete tool, so the offer was a
hallucination. External clients never see the internal agent prompt;
the only text they read is the tool descriptions and the agent/ask_postiz
description, so the guard has to live there:

- postsListTool description: posts cannot be deleted through these
  tools; never offer deletion, point the user to the Postiz app.
- Agent description: "manage and schedule" -> "schedule and list", so
  external clients don't infer edit/delete capabilities from "manage".

Validated: re-ran the same flow in Claude Code, claude.ai and ChatGPT
over MCP - no delete offers.

libraries/nestjs-libraries/src/chat/tools/posts.list.tool.ts
@@ -39,6 +39,7 @@ List the organization's posts scheduled to be published between two dates (the s
 Returns every post in the window whatever its state (scheduled, draft, published, errored).
 "startDate" and "endDate" are required (UTC) - to list all upcoming posts, pass a wide window (for example from now to a year ahead).
 Each item has an "id", its publish date, state, content, channel and current provider settings.
+Posts cannot be deleted through the Postiz tools - if the user wants to delete a post, tell them to do it themselves in the Postiz app; never offer to delete a post.
 `,
       inputSchema: z.object({
         startDate: z

GkhanKINAY/postqueen-app

(cherry picked from commit 0321796ed474a9e14da670f01e8a3216539d6df0)

libraries/nestjs-libraries/src/chat/tools/posts.list.tool.ts
@@ -39,6 +39,7 @@ List the organization's posts scheduled to be published between two dates (the s
 Returns every post in the window whatever its state (scheduled, draft, published, errored).
 "startDate" and "endDate" are required (UTC) - to list all upcoming posts, pass a wide window (for example from now to a year ahead).
 Each item has an "id", its publish date, state, content, channel and current provider settings.
+Posts cannot be deleted through the PostQueen tools - if the user wants to delete a post, tell them to do it themselves in the PostQueen app; never offer to delete a post.
 `,
       inputSchema: z.object({
         startDate: z