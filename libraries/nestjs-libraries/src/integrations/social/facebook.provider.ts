gitroomhq/postiz-app

fix(facebook): flag channels that hold a non-page token as needing reconnection
A Facebook channel can end up holding the user's token instead of the
page's: if the page is only visible through a Business portfolio and was
never ticked in the OAuth page list, Graph returns no access_token for
it, fetchPageInformation passes undefined to the update, and Prisma
leaves the between-steps user token in place. The channel then looks
connected while every publish is rejected with "(#200) Unpublished posts
must be posted to a page as the page itself".

That error was unmapped, so it surfaced as a generic "Unknown Error" on
every post while the channel kept reporting as healthy - the user had no
way to tell a token problem from a content problem. Map it to
refresh-token so the channel is flagged, the reconnect notification is
sent, and the post is not retried against a token that can never work.

Co-Authored-By: Claude Opus 5 (1M context) <noreply@anthropic.com>

libraries/nestjs-libraries/src/integrations/social/facebook.provider.ts
@@ -81,6 +81,20 @@ export class FacebookProvider extends SocialAbstract implements SocialProvider {
       };
     }
 
+    // The token is valid but belongs to the user, not to the page - the page
+    // was never granted to the app, so only reconnecting can fix it
+    if (
+      body.indexOf(
+        'Unpublished posts must be posted to a page as the page itself'
+      ) > -1
+    ) {
+      return {
+        type: 'refresh-token' as const,
+        value:
+          'Postiz is not authorized to publish as this page, please reconnect the channel',
+      };
+    }
+
     if (body.indexOf('1366046') > -1) {
       return {
         type: 'bad-body' as const,

GkhanKINAY/postqueen-app

Adapted for this fork: the reconnect message names PostQueen, not Postiz.
(cherry picked from commit 8bcb2f548b665b2e443610ed354691a4fcc240ae)

libraries/nestjs-libraries/src/integrations/social/facebook.provider.ts
@@ -90,6 +90,20 @@ export class FacebookProvider extends SocialAbstract implements SocialProvider {
       };
     }
 
+    // The token is valid but belongs to the user, not to the page - the page
+    // was never granted to the app, so only reconnecting can fix it
+    if (
+      body.indexOf(
+        'Unpublished posts must be posted to a page as the page itself'
+      ) > -1
+    ) {
+      return {
+        type: 'refresh-token' as const,
+        value:
+          'PostQueen is not authorized to publish as this page, please reconnect the channel',
+      };
+    }
+
     if (body.indexOf('1366046') > -1) {
       return {
         type: 'bad-body' as const,